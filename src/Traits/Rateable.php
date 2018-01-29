88100047f24cfdc0972758f1140dc4b14b46ebf7
Younes Khoubza <nesyou2013@gmail.com>
2018-01-29 23:49:06 +0000

    short hand for get_class($this)

--- src/Traits/Rateable.php
+++ src/Traits/Rateable.php
@@ -115,7 +115,7 @@ trait Rateable
             ->leftJoin('ratings', function (JoinClause $join) {
                 $join
                     ->on('ratings.rateable_id', $this->getTable() . '.id')
-                    ->where('ratings.rateable_type', Relation::getMorphedModel(get_class($this)) ?? get_class($this));
+                    ->where('ratings.rateable_type', Relation::getMorphedModel(__CLASS__) ?? __CLASS__);
             })
             ->addSelect(DB::raw('AVG(ratings.value) as average_rating'))
             ->groupBy($this->getTable(). '.id')
